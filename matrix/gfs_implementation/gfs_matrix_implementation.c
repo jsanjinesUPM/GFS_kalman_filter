837c4f52d648d6ebb626fe57b7221ae55547323c
Joel Sanjines Urquiola <joel.sanjines.urquiola@alumnos.upm.es>
2023-10-02 23:53:38 +0200

    gfs_matrix_implementation.c: sytnax fixed. Redeclaration of result

--- matrix/gfs_implementation/gfs_matrix_implementation.c
+++ matrix/gfs_implementation/gfs_matrix_implementation.c
@@ -10,7 +10,7 @@ struct gfs_matrix* multiplyMatrices(const struct gfs_matrix* matrixA, const stru
         goto OUT;
     }
 
-    struct gfs_matrix* result = createMatrix(matrixA->rows, matrixB->cols);
+    result = createMatrix(matrixA->rows, matrixB->cols);
 
     if (result == NULL) {
         goto OUT;
@@ -28,4 +28,70 @@ struct gfs_matrix* multiplyMatrices(const struct gfs_matrix* matrixA, const stru
 
 OUT:
     return result;
+}
+
+struct gfs_matrix* createMatrix(uint32_t rows, uint32_t cols) {
+    
+    struct gfs_matrix* matrix = (struct gfs_matrix*)malloc(sizeof(struct gfs_matrix));
+
+    if (!matrix) 
+        goto OUT; 
+    
+    matrix->rows = rows;
+    matrix->cols = cols;
+ 
+    matrix->data = (double**)malloc(rows * sizeof(double*));
+
+    if (matrix->data == NULL) {
+        free(matrix);
+        matrix = NULL;
+        goto OUT; 
+    }
+
+    for (uint32_t row = 0; row < rows; row++) {
+        matrix->data[row] = (double*)malloc(cols * sizeof(double));
+
+        if (matrix->data[row] == NULL) {
+            
+            for (uint32_t j = 0; j < row; j++) 
+                free(matrix->data[j]);
+            
+            free(matrix->data);
+            free(matrix);
+            matrix = NULL;
+            goto OUT;;
+        }
+    }
+
+OUT:
+    return matrix;
+}
+
+void freeMatrix(struct gfs_matrix* matrix) {
+    if (!matrix)
+        return;
+
+    for (uint32_t i = 0; i < matrix->rows; i++) {
+        if (matrix->data[i])
+            free(matrix->data[i]);
+    }
+
+    free(matrix->data);
+    free(matrix);
+}
+
+void printMatrix(const struct gfs_matrix* matrix) {
+    if (!matrix) {
+        printf("Matrix not initialized\n");
+        return;
+    }
+
+    printf("Matrix (%u x %u):\n", matrix->rows, matrix->cols);
+
+    for (uint32_t row = 0; row < matrix->rows; row++) {
+        for (uint32_t column = 0; column < matrix->cols; column++) {
+            printf("%f ", matrix->data[row][column]);
+        }
+        printf("\n");
+    }
 }
