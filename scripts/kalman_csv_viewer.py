13f36f5a9dfc7714e694422d6fd1d970144ba2ff
MateoMiguelez <mateomiguelezdesalas@gmail.com>
2023-10-20 19:26:56 +0200

    Issue#1. Kalman example working fine. Script to view results added

---
+++ scripts/kalman_csv_viewer.py
@@ -0,0 +1,23 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../build/kalman_results.csv', skip_blank_lines=True, header=0)
+
+fig, ax = plt.subplots(2,2)
+ax[0,0].plot(df['time'], df['h_position'], color='green', label="Estimated X")
+ax[0,0].plot(df['time'], df['real_h_pos'], color='red', label="Real X")
+ax[0,0].legend()
+ax[1,0].plot(df['time'], df['v_position'], color='green', label="Estimated Y")
+ax[1,0].plot(df['time'], df['real_v_pos'], color='red', label="Real Y")
+df['measured_v'] = df['measured_v'].replace({0:np.nan})
+ax[1,0].scatter(df['time'], df['measured_v'], color='black', label="Measurements")
+ax[1,0].legend()
+ax[0,1].plot(df['time'], df['h_velocity'], color='green', label="Estimated X Velocity")
+ax[0,1].plot(df['time'], df['real_h_vel'], color='red', label="Real X Velocity")
+ax[0,1].legend()
+ax[1,1].plot(df['time'], df['v_velocity'], color='green', label="Estimated Y Velocity")
+ax[1,1].plot(df['time'], df['real_v_vel'], color='red', label="Real Y Velocity")
+ax[1,1].legend()
+plt.show()
--- tests/math_lib_tests.c
+++ tests/math_lib_tests.c
@@ -17,9 +17,9 @@ int main(void) {
     //Result will be written to a file
     FILE *fpt;
     fpt = fopen("kalman_results.csv", "w+");
-    fprintf(fpt, "time, h_position, v_position, h_velocity, v_velocity, measurement,"
-                    " pi_h_pos, pi_v_pos, pi_h_vel, pi_v_vel, real_h_pos, real_v_pos,"
-                    "real_h_vel, real_v_vel\n"); //CSV file Header
+    fprintf(fpt, "time,h_position,v_position,h_velocity,v_velocity,measurement,"
+                    "pi_h_pos,pi_v_pos,pi_h_vel,pi_v_vel,real_h_pos,real_v_pos,"
+                    "real_h_vel,real_v_vel,measured_angle,measured_v\n"); //CSV file Header
 
     int Nx = 4;
     int Ny = 2;
@@ -51,8 +51,8 @@ int main(void) {
     //Give matrices inicial value
     x_0.d[0]= 0;
     x_0.d[1]= 0;
-    x_0.d[2]= 20;
-    x_0.d[3]= 60;
+    x_0.d[2]= 30;
+    x_0.d[3]= 50;
 
     u.d[0] = 9.8;
 
@@ -60,19 +60,24 @@ int main(void) {
     F.d[0][2] = time_delta;
     F.d[1][3] = time_delta;
                     //      [-y/x^2+y^2  x/x^2+y^2   0   0]
-    H.d[1][2] = 1;  // H =  [     0          1       0   0]
+    H.d[1][1] = 1;  // H =  [     0          1       0   0]
 
     G.d[1][0] = -time_delta*time_delta/2;   //      [0      0]
     G.d[3][0] = -time_delta;                // G =  [-t^2/2 0]
                                             //      [0      0]
-    R.d[0][0] = 0.05;                       //      [-t     0]
-    R.d[0][1] = 0.5;
-    R.d[1][1] = 0.5;
-    R.d[1][1] = 5;
+    // R.d[0][0] = 0.05;                       //      [-t     0]
+    // R.d[0][1] = 0.5;
+    // R.d[1][1] = 0.5;
+    // R.d[1][1] = 5;
+    R.d[0][0] = 0.0001;
+    R.d[0][1] = 0.0001;
+    R.d[1][1] = 0.0001;
+    R.d[1][1] = 0.0001;
 
     rc_matrix_t tmp1 = RC_MATRIX_INITIALIZER;
     rc_matrix_transpose(G, &tmp1);
     rc_matrix_multiply(G, tmp1, &Q);
+    rc_matrix_times_scalar(&Q,100);
     rc_matrix_free(&tmp1);
 
     Pi.d[0][0] = 0.1;
@@ -83,8 +88,11 @@ int main(void) {
     //We need to calculate "real" values to generate measurements and compare results
     double real_h_pos = 0;
     double real_v_pos = 0;
-    double real_h_vel = 30;
-    double real_v_vel = 50;
+    double real_h_vel = 20;
+    double real_v_vel = 60;
+
+    double measured_angle = 0;
+    double measured_v = 0;
 
     //Variables to update H
     double h_pos = 0;
@@ -92,29 +100,36 @@ int main(void) {
 
     if(rc_kalman_new_alloc(&kf, F, G, H, Q, R, Pi, x_0)==-1) return -1;
 
-    for(int i = 0; i <= 10000; i++){
+    for(int i = 0; i <= 1000; i++){
         if(rc_kalman_predict_ekf(&kf, F, G, u) == -1) return -1;
         if(i % 100 == 0){
             y.d[0] = atan2(real_v_pos, real_h_pos) + ((rand()%1000)/1000 - 0.5)*sqrt(R.d[0][0]);
             y.d[1] = real_v_pos + ((rand()%1000)/1000 - 0.5)*sqrt(R.d[1][1]);
             if(rc_kalman_prediction_update_ekf(&kf, H, y) == -1) return -1;
+
+            measured_angle = y.d[0];
+            measured_v = y.d[1];
         }
-        fprintf(fpt,"%f, %f, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
+        fprintf(fpt,"%f, %f, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
                                         time_delta*i ,kf.x_pre.d[0], kf.x_pre.d[1], kf.x_pre.d[2],
                                         kf.x_pre.d[3], y.d[0], kf.P.d[0][0], kf.P.d[1][1],
                                         kf.P.d[2][2], kf.P.d[3][3], real_h_pos, real_v_pos, 
-                                        real_h_vel, real_v_vel);
+                                        real_h_vel, real_v_vel, measured_angle, measured_v);
         // Update H
         h_pos = kf.x_pre.d[0];
         v_pos = kf.x_pre.d[1];
         H.d[0][0] = -v_pos/((h_pos*h_pos)+(v_pos*v_pos));
-        H.d[0][0] = h_pos/((h_pos*h_pos)+(v_pos*v_pos));
+        H.d[1][0] = h_pos/((h_pos*h_pos)+(v_pos*v_pos));
 
         //Update Real Values
         real_h_pos = 0 + real_h_vel*time_delta*i;
-        real_v_pos = 0 + real_v_vel*time_delta*i - 9.8*0.5*time_delta*i ;
+        real_v_pos = 0 + 50*time_delta*i - 9.8*0.5*time_delta*time_delta*i*i ;
         real_h_vel = 30;
         real_v_vel = 50 - 9.8*time_delta*i;
+
+        //Reset Measurements for writing to file purposes
+        measured_angle = 0;
+        measured_v = 0;
     }
 
     fclose(fpt);
